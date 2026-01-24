32d24fe9f25b5c8fdfb2e95e874a073bce63d461
Pericles Junior <184568632+PericlesJunior1212@users.noreply.github.com>
2026-01-24 01:09:46 -0300

    ok

--- venturegotchi/settings.py
+++ venturegotchi/settings.py
@@ -96,11 +96,13 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+        "default": dj_database_url.config(
+            default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
+            conn_max_age=600,
+            ssl_require=False,
+    )
+}
+
 else:
    
     DATABASES = {
