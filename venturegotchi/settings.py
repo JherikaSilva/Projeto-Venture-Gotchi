360f5b3ecbc2be44c43031d556c1cc51374c1349
Pericles Junior <184568632+PericlesJunior1212@users.noreply.github.com>
2026-01-24 13:19:11 -0300

    render

--- venturegotchi/settings.py
+++ venturegotchi/settings.py
@@ -112,8 +112,17 @@ else:
             ssl_require=True,
         )
     }
-
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DATABASE_NAME'),
+            'USER': os.environ.get('DATABASE_USER'),
+            'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+            'HOST': os.environ.get('DATABASE_HOST'),
+            'PORT': os.environ.get('DATABASE_PORT', 5432),
+    }
+}
 ALLOWED_HOSTS = ["*", ".onrender.com"]
 
 
