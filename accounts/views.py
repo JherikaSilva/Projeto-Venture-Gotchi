04cbd67a17df44001f4dde730dc9ef26274abc8a
JherikaSilva <pereirajherika@gmail.com>
2026-01-26 15:40:03 -0300

    .

--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-
 from .forms import RegisterForm, ProfileUpdateForm, LoginForm
+
 
 logger = logging.getLogger(__name__)
 
@@ -45,7 +45,7 @@ class HomeView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
-
+     
     
         xp = getattr(user, 'xp', 0)
         level = getattr(user, 'level', 1)
