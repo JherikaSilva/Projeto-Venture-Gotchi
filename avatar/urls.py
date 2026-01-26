4e942fc498026934754f84f0d45970819fc32c6b
JherikaSilva <pereirajherika@gmail.com>
2026-01-26 15:40:14 -0300

    removi duplicadas

--- avatar/urls.py
+++ avatar/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-from .views import avatar_view
+
 urlpatterns = [
     path("", views.avatar_view, name="avatar"),
     path("buy/<int:item_id>/", views.buy_item, name="avatar_buy"),
     path("equip/<int:item_id>/", views.equip_item, name="avatar_equip"),
     path("unequip/<str:slot>/", views.unequip_slot, name="avatar_unequip"),
-    path("", avatar_view, name="avatar")
 ]
