6043d21625693ae286402ed982e8cf7bb1a6c798
Pericles Junior <184568632+PericlesJunior1212@users.noreply.github.com>
2026-01-26 21:37:38 -0300

    alinhando metas trilhas e equipes nesses arquivos

--- orgs/urls.py
+++ orgs/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path
 from .views import team_dashboard
+from .views import (
+    team_dashboard,
+    corporate_track_create,
+    corporate_track_list,
+    assign_track_to_team,
+    corporate_mission_create,
+    corporate_mission_complete,
+)
+
+
 
 urlpatterns = [
     path("team/", team_dashboard, name="team_dashboard"),
+    path("tracks/", corporate_track_list, name="corporate_track_list"),
+    path("tracks/new/", corporate_track_create, name="corporate_track_create"),
+    path("tracks/<int:track_id>/assign/", assign_track_to_team, name="assign_track_to_team"),
+    path("tracks/<int:track_id>/missions/new/",corporate_mission_create,name="corporate_mission_create"),
+    path("missions/<int:mission_id>/complete/",corporate_mission_complete,name="corporate_mission_complete"),
+
 ]
